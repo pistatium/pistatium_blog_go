f4c22061c70ffc32247f5115dbe03d5a2306d74b
Kimihiro N <kimihiro.nisimura@gmail.com>
2019-10-31 19:12:43 +0900

    Datastoreの開発環境を構築

--- blog.go
+++ blog.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"context"
 	"os"
 	"fmt"
 	"time"
 	"log"
 	"net/http"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/datastore"
+	"cloud.google.com/go/datastore"
 )
 
 type Blog struct {
@@ -22,26 +22,33 @@ type Blog struct {
 
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	q := datastore.NewQuery("Blog").Order("-datetime").Limit(10)
-	es := make([]Blog, 0, 10)
-	if _, err := q.GetAll(c, &es); err != nil {
+	ctx := context.Background()
+	projectID := os.Getenv("DATASTORE_PROJECT_ID")
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	q := datastore.NewQuery("Blog").Order("-datetime").Limit(10)
+
+	// FIXME: ページング
+	es := make([]Blog, 0, 10)
+	if _, err := client.GetAll(ctx, q, &es); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	fmt.Fprintf(w, "%v+", es)
 }
 
 func main() {
 	http.HandleFunc("/", rootHandler)
 
-	appengine.Main()
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-		log.Printf("Defaulting to port %s", port)
 	}
-
 	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pistatium/pistatium_blog_go
 
 go 1.11
 
-require google.golang.org/appengine v1.6.5
+require (
+	cloud.google.com/go/datastore v1.0.0
+	google.golang.org/appengine v1.6.5 // indirect
+)
