79e79be9784890258afb86f4e26842212625fede
Kimihiro N <kimihiro.nisimura@gmail.com>
2019-11-15 09:34:42 +0900

    fix design

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app id="app">
         <Navigation></Navigation>
-        <v-container grid-list-lg style="margin-top: 64px;">
+        <v-container grid-list-lg>
             <router-view></router-view>
         </v-container>
     </v-app>
@@ -24,3 +24,10 @@
         mounted () {},
     }
 </script>
+
+<style scoped>
+    .container {
+        margin-top: 48px;
+        padding: 0px;
+    }
+</style>
--- front/src/pages/Entries.vue
+++ front/src/pages/Entries.vue
@@ -1,16 +1,23 @@
 <template>
     <div>
-        <v-layout row wrap>
+        <v-layout row wrap v-if="!empty">
             <v-flex xs12>
-                <Entry v-for="entry in entries" v-bind:key="entry.id" v-bind:entry=entry v-bind:show_detail="false"></Entry>
+                <Entry v-for="entry in entries" v-bind:key="entry.id" v-bind:entry=entry
+                       v-bind:show_detail="false"></Entry>
             </v-flex>
         </v-layout>
+        <v-alert v-else
+                 outlined
+                 prominent
+                 border="left">
+            これ以上記事はありません
+        </v-alert>
 
         <div class="text-center">
             <v-btn class="ma-2" tile color="green" dark v-bind:to=prev_page v-if="page > 0">&lt;&lt; Newer
             </v-btn>
             <v-btn class="ma-2" tile color="green" dark to="/" v-if="page !== 0">^ Top</v-btn>
-            <v-btn class="ma-2" tile color="green" dark v-bind:to=next_page>&gt;&gt; Older</v-btn>
+            <v-btn class="ma-2" tile color="green" dark v-bind:to=next_page v-if="!empty">&gt;&gt; Older</v-btn>
 
         </div>
     </div>
@@ -20,40 +27,54 @@
     import axios from 'axios';
     import Entry from "../components/Entry";
 
+
     export default {
         name: 'Entries',
         components: {Entry},
         data: () => ({
             entries: [],
             page: 0,
+            empty: false,
         }),
-        mounted () {
-            this.page = parseInt(this.$route.params.page, 10) || 0
-            axios.get('/api/entries?page=' + this.page).then(res => {
-                this.entries = res.data.entries;
-                console.log(res);
-            })
+        mounted() {
+            this._fetchEntries()
         },
         watch: {
-            '$route' (to, from) {
-                this.page = parseInt(this.params.page, 10) || 0
-                axios.get('/api/entries?page=' + this.page).then(res => {
-                    this.entries = res.data.entries;
-                    console.log(res);
-                })
+            '$route'(to, from) {
+                console.log(this._fetchEntries())
+                this._fetchEntries()
             }
         },
         computed: {
-            prev_page: function() {
+            prev_page: function () {
                 if (this.page === 1) {
                     return '/'
                 }
                 return `/${this.page - 1}`
             },
-            next_page: function() {
+            next_page: function () {
                 return `/${this.page + 1}`
             },
+        },
+        methods: {
+            _fetchEntries () {
+                this.empty = false
+                this.page = parseInt(this.$route.params.page, 10) || 0
+                axios.get('/api/entries?page=' + this.page).then(res => {
+                    this.entries = res.data.entries;
+                    if (this.entries.length == 0) {
+                        this.empty = true
+                    }
+                })
+            }
         }
+
 
     }
 </script>
+<style scoped>
+    .v-alert {
+        margin: 32px;
+        padding: 32px;
+    }
+</style>
