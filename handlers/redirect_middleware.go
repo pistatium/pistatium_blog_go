4be4cef2ac9662e0af8fb7dd5b8de473a6767b24
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-12-27 21:59:16 +0900

    Delete log

--- handlers/redirect_middleware.go
+++ handlers/redirect_middleware.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"os"
 )
 
 const defaultAppEngineDomain = "kimihiro-n.appspot.com"
@@ -11,8 +10,6 @@ const redirectTo = "https://pistatium.dev"
 
 func DomainRedirectMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		println("hostname req", c.Request.Host)
-		println("hostname env", os.Getenv("HTTP_HOST"))
 		if c.Request.Host == defaultAppEngineDomain {
 			c.Redirect(http.StatusPermanentRedirect, redirectTo + c.Request.URL.Path)
 			return
